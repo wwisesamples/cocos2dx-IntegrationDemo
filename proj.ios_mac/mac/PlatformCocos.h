cc7e2eecf839382fd05913151e8a97c11853efe8
Beinan Li <li.beinan@gmail.com>
2018-02-03 15:58:17 +0800

    Refactor: Removed files unsupposed to be SCM'ed. - Removed Xcode workspace and user data. - Updated ignore patterns.

---
+++ proj.ios_mac/mac/PlatformCocos.h
@@ -0,0 +1,15 @@
+// Platform.h
+// Copyright (C) 2011 Audiokinetic Inc
+/// \file 
+/// Contains platform-specific helpers for the iOS platform.
+
+#pragma once
+
+#ifdef SOUND_BANK_PATH
+#undef SOUND_BANK_PATH
+#define SOUND_BANK_PATH L"../../WwiseProject/GeneratedSoundBanks/Mac/"
+#endif
+
+#define FONT_SIZE_TITLE  30
+#define FONT_SIZE_MENU   24
+
