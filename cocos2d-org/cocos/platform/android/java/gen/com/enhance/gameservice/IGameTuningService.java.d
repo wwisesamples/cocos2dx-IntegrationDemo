cbe9d4d69eac7ea4359d4ed17390e8939cdd05a6
Beinan Li <li.beinan@gmail.com>
2018-02-04 22:40:19 +0800

    added missing files.

--- cocos2d-org/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java.d
+++
@@ -1,3 +0,0 @@
-D:\Audiokinetic\Wwise_v2015.1.4_build_5497\SDK\samples\IntegrationDemo\cocos2d-x\cocos2d\cocos\platform\android\java\gen\com\enhance\gameservice\IGameTuningService.java: \
-  D:\Audiokinetic\Wwise_v2015.1.4_build_5497\SDK\samples\IntegrationDemo\cocos2d-x\cocos2d\cocos\platform\android\java\src\com\enhance\gameservice\IGameTuningService.aidl 
-
