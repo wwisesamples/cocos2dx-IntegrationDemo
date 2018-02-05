34097f9afc5b61565badce277a768bf0754799fb
Beinan Li <li.beinan@gmail.com>
2018-02-05 19:57:40 +0800

    Added: Android platform header for Cocos demo.

---
+++ proj.android/PlatformCocos.h
@@ -0,0 +1,16 @@
+// Platform.h
+// Copyright (C) 2010 Audiokinetic Inc
+/// \file 
+/// Contains platform-specific helpers for the Windows platform.
+
+#pragma once
+
+#ifdef SOUND_BANK_PATH
+#undef SOUND_BANK_PATH
+#define SOUND_BANK_PATH "GeneratedSoundBanks/Android/"
+#endif
+
+
+#define FONT_SIZE_TITLE	    16
+#define FONT_SIZE_MENU	    10
+
