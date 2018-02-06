9d64de321265bff2be52c93b22077c241f0e28f4
Beinan Li <li.beinen@gmail.com>
2018-02-06 20:42:36 +0800

    Refactor: Apply same cross-platform change to Windows.

--- proj.win32/PlatformCocos.h
+++ proj.win32/PlatformCocos.h
@@ -27,5 +27,6 @@ written agreement between you and Audiokinetic Inc.
 #define FONT_SIZE_TITLE	    30
 #define FONT_SIZE_MENU	    24
 
+extern bool g_isLandscape;
 #define SCREEN_POS_SCALER_DESCRIPTION_Y 0.90f
 #define SCREEN_POS_SCALER_SELBUTTON_X 0.15f
